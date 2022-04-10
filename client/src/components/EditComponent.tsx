3b807cbc6cf6ac687fe9b19ab66856437e9afacf
VarunLanjhara <lanjharav@gmail.com>
2022-04-11 01:34:09 +0530

    Delete and update task stuf

---
+++ client/src/components/EditComponent.tsx
@@ -0,0 +1,41 @@
+import { Button, IconButton, Input, Textarea } from "@chakra-ui/react";
+import React from "react";
+import { FaTags } from "react-icons/fa";
+import { BsFlagFill } from "react-icons/bs";
+
+const EditComponent = () => {
+  return (
+    <div className="flex w-full flex-col gap-5">
+      <div
+        className="flex w-full flex-col rounded-2xl p-4 gap-2"
+        style={{
+          boxShadow:
+            "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px",
+          border: "1px solid #e6e6e6",
+        }}
+      >
+        <Input
+          variant="unstyled"
+          placeholder="Task title"
+          defaultValue="Dumb"
+        />
+        <Textarea
+          placeholder="Title description"
+          variant="unstyled"
+          resize="none"
+          defaultValue="I am dumb help"
+        />
+        <div className="flex items-center gap-4">
+          <IconButton icon={<FaTags />} aria-label="Tags" />
+          <IconButton icon={<BsFlagFill />} aria-label="Priority" />
+        </div>
+      </div>
+      <div className="flex items-center gap-4 mb-4">
+        <Button colorScheme="blue">Update Task</Button>
+        <Button variant="outline">Cancel</Button>
+      </div>
+    </div>
+  );
+};
+
+export default EditComponent;
--- client/src/components/TaskComponent.tsx
+++ client/src/components/TaskComponent.tsx
@@ -4,61 +4,116 @@ import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { MdDragIndicator } from "react-icons/md";
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+import EditComponent from "./EditComponent";
 
 const TaskComponent = () => {
   const [hover, setHover] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef(null);
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <div className="flex flex-col w-full">
-      <div
-        className="flex justify-between cursor-pointer w-full"
-        onMouseEnter={() => {
-          setHover(true);
-        }}
-        onMouseLeave={() => {
-          setHover(false);
-        }}
-      >
-        <div className="flex gap-3 items-start mb-4">
-          <Checkbox className="mt-1" />
-          <div className="flex flex-col gap-1">
-            <Text fontSize="md">Eat shit</Text>
-            <Text fontSize="md" color="gray.500">
-              Why eating shit is good for health
-            </Text>
+      {!isEdit ? (
+        <div
+          className="flex justify-between cursor-pointer w-full"
+          onMouseEnter={() => {
+            setHover(true);
+          }}
+          onMouseLeave={() => {
+            setHover(false);
+          }}
+        >
+          <div className="flex gap-3 items-start mb-4">
+            <Checkbox className="mt-1" />
+            <div className="flex flex-col gap-1">
+              <Text fontSize="md">Eat shit</Text>
+              <Text fontSize="md" color="gray.500">
+                Why eating shit is good for health
+              </Text>
+            </div>
           </div>
+          {hover ? (
+            <div className="flex gap-4 items-center">
+              <IconButton icon={<MdDragIndicator />} aria-label="Drag" />
+              <IconButton icon={<AiOutlineEdit />} aria-label="Edit" />
+              <Menu>
+                <MenuButton>
+                  <IconButton
+                    icon={<FiMoreHorizontal />}
+                    aria-label="More options"
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem
+                    className="flex items-center gap-3"
+                    onClick={() => {
+                      setIsEdit(true);
+                    }}
+                  >
+                    <AiOutlineEdit size="20" color="#9bdaf3" />
+                    <Text fontSize="md" color="#9bdaf3">
+                      Edit task
+                    </Text>
+                  </MenuItem>
+                  <MenuItem
+                    className="flex items-center gap-3"
+                    onClick={() => {
+                      onOpen();
+                    }}
+                  >
+                    <AiOutlineDelete size="20" color="#df3333" />
+                    <Text fontSize="md" color="#df3333">
+                      Delete task
+                    </Text>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-        {hover ? (
-          <div className="flex gap-4 items-center">
-            <IconButton icon={<MdDragIndicator />} aria-label="Drag" />
-            <IconButton icon={<AiOutlineEdit />} aria-label="Edit" />
-            <Menu>
-              <MenuButton>
-                <IconButton
-                  icon={<FiMoreHorizontal />}
-                  aria-label="More options"
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem className="flex items-center gap-3">
-                  <AiOutlineEdit size="20" color="#9bdaf3" />
-                  <Text fontSize="md" color="#9bdaf3">
-                    Edit task
-                  </Text>
-                </MenuItem>
-                <MenuItem className="flex items-center gap-3">
-                  <AiOutlineDelete size="20" color="#df3333" />
-                  <Text fontSize="md" color="#df3333">
-                    Delete task
-                  </Text>
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+      ) : (
+        <EditComponent />
+      )}
       <Divider />
+      {/* delete task modal stuff here */}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Task
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button onClick={onClose} ref={cancelRef}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </div>
   );
 };
