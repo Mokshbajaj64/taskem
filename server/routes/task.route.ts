282d0d34bad8a1e1524a6be34f8ad541eb34bc48
VarunLanjhara <lanjharav@gmail.com>
2022-04-12 23:14:02 +0530

    Weekly task api stuff

--- server/routes/task.route.ts
+++ server/routes/task.route.ts
@@ -281,4 +281,142 @@ router.put(
   }
 );
 
+//create a weeklytask
+router.post(
+  '/weekly',
+  body('title')
+    .trim()
+    .exists()
+    .withMessage('Title is required')
+    .isLength({
+      min: 1,
+      max: 100,
+    })
+    .withMessage('Title must be between 1 and 100 characters'),
+  isAuthenticated,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = res?.locals?.userId;
+      const data: TaskModel = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({
+          error: errors.array()[0].msg,
+        });
+      } else {
+        const task = await Task.create({
+          title: data?.title,
+          description: data?.description,
+          userId: userId,
+          isTodayTask: false,
+          isInboxTask: false,
+          isWeeklyTask: true,
+        });
+        res.json(task);
+      }
+    } catch (error: any) {
+      res.json({
+        error: error.message,
+      });
+    }
+  }
+);
+
+//get all weeklytasks
+router.get('/weekly', isAuthenticated, async (req: Request, res: Response) => {
+  try {
+    const userId = res?.locals?.userId;
+    const tasks = await Task.find({
+      userId: userId,
+      isWeeklyTask: true,
+    });
+    res.json(tasks);
+  } catch (error: any) {
+    res.json({
+      error: error.message,
+    });
+  }
+});
+
+//delete a weeklytask
+router.delete(
+  '/weekly/:id',
+  isAuthenticated,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = res?.locals?.userId;
+      const task = await Task.findOne({
+        _id: req?.params?.id,
+        userId: userId,
+        isWeeklyTask: true,
+      });
+      if (task === null) {
+        res.json({
+          error: 'Task not found',
+        });
+      } else {
+        await Task.findByIdAndDelete(req?.params?.id);
+        res.json('Task deleted');
+      }
+    } catch (error: any) {
+      res.json({
+        error: error.message,
+      });
+    }
+  }
+);
+
+//update a weeklytask
+router.put(
+  '/weekly/:id',
+  body('title')
+    .trim()
+    .exists()
+    .withMessage('Title is required')
+    .isLength({
+      min: 1,
+      max: 100,
+    })
+    .withMessage('Title must be between 1 and 100 characters'),
+  isAuthenticated,
+  async (req: Request, res: Response) => {
+    try {
+      const data: TaskModel = req.body;
+      const userId = res?.locals?.userId;
+      const task = await Task.findOne({
+        _id: req?.params?.id,
+        userId: userId,
+        isWeeklyTask: true,
+      });
+      if (task === null) {
+        res.json({
+          error: 'Task not found',
+        });
+      } else {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.json({
+            error: errors.array()[0].msg,
+          });
+        } else {
+          await Task.findByIdAndUpdate(req?.params?.id, {
+            title: data?.title,
+            description: data?.description,
+          });
+          const updatedTask = await Task.findOne({
+            _id: req?.params?.id,
+            userId: userId,
+            isWeeklyTask: true,
+          });
+          res.json(updatedTask);
+        }
+      }
+    } catch (error: any) {
+      res.json({
+        error: error.message,
+      });
+    }
+  }
+);
+
 export default router;
